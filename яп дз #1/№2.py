cc1299572b131e83b5e89579f7fd3658a360bb45
Nikita1234644 <143877370+Nikita1234644@users.noreply.github.com>
2023-09-23 10:37:45 +0300

    Add files via upload

---
+++ яп дз #1/№2.py
@@ -0,0 +1,12 @@
+# «Делить на ноль нельзя!».
+a=float(input('a='))
+b=float(input('b='))
+c=float(input('c='))
+d=float(input('d='))
+f=float(input('f='))
+if (f-d)!=0:
+    rep = (a*b-c)/(f-d)
+    print(rep)
+else:
+    print("Делить на ноль нельзя!")
+
