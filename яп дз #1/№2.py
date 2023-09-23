1fef99d14ec6c7ff1970c8210219012f659268e2
Nikita1234644 <143877370+Nikita1234644@users.noreply.github.com>
2023-09-23 11:18:12 +0300

    Update №2.py

--- яп дз #1/№2.py
+++ яп дз #1/№2.py
@@ -5,8 +5,7 @@ c = float(input('c='))
 d = float(input('d='))
 f = float(input('f='))
 if f-d != 0:
-    rep = (a*b-c)/(f-d)
+    rep = (a*b-c) / (f-d)
     print(rep)
 else:
-    print ("Делить на ноль нельзя!")
-
+    print("Делить на ноль нельзя!")
