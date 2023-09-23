296404cb6d03117f76c0c2f149ea043fa8a4fe69
Nikita1234644 <143877370+Nikita1234644@users.noreply.github.com>
2023-09-23 11:08:42 +0300

    Update №5.py

--- яп дз #1/№5.py
+++ яп дз #1/№5.py
@@ -1,17 +1,17 @@
 # Определить в какой четверти находится точка 
 y=float(input('x:'))
 x=float(input('y:'))
-if y==0:
+if y == 0:
     print('Точка на оси ординат')
-elif x==0:
+elif x == 0:
     print('Точка на оси абсцисс')
-elif y>0 and x>0:
+elif y > 0 and x > 0:
     print('Точка в 1-ой четверти')
-elif y>0 and x<0:
+elif y > 0 and x < 0:
     print('Точка во 2-ой четверти')
-elif y<0 and x<0:
+elif y < 0 and x < 0:
     print('Точка в 3-ей четверти')
-elif y<0 and x>0:
+elif y < 0 and x > 0:
     print('Точка в 4-ой четверти')
 else:
     print('Точка в начале координат')
