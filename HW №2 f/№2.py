0c1703b5ec865478695691f4f5fbdc0e8615f8a3
Nikita1234644 <143877370+Nikita1234644@users.noreply.github.com>
2023-10-07 14:38:20 +0300

    Update №2.py

--- HW №2 f/№2.py
+++ HW №2 f/№2.py
@@ -3,5 +3,5 @@ y = input("Введите количество целых чисел: ")
 while y:    
     x.append(y)
     y = input("Введите число: ")                       
-    x.sort(reverse=True)               
+x.sort(reverse=True)               
 print("".join(x))   
