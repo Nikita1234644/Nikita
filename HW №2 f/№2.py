981b0c279f5a09e61eddb19af3a3f7eb9eb4a43e
Nikita1234644 <143877370+Nikita1234644@users.noreply.github.com>
2023-10-07 14:35:10 +0300

    Add files via upload

---
+++ HW №2 f/№2.py
@@ -0,0 +1,7 @@
+x= []                                  
+y = input("Введите количество целых чисел: ")
+while y:    
+    x.append(y)
+    y = input("Введите число: ")                       
+    x.sort(reverse=True)               
+print("".join(x))   
