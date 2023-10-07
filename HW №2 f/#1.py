d3cb4be387b27886f65f82e7aa255f46c945dc30
Nikita1234644 <143877370+Nikita1234644@users.noreply.github.com>
2023-10-07 12:23:37 +0300

    Add files via upload

---
+++ HW №2 f/#1.py
@@ -0,0 +1,7 @@
+elements = []                                
+while True:                                  # запустил бесконечный цикл       
+    user_input = input("Введите элемент: ")  
+    if user_input == "":                     # проверяем пустая ли строка 
+        break                                # да - прерываем цикл с помощью break
+    elements.append(user_input)              # нет - значение добавляется в конец списка
+print("Итоговый список:", elements)           
