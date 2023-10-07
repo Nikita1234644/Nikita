a98c9349e160d895fde6b938201e45a1987a8024
Nikita1234644 <143877370+Nikita1234644@users.noreply.github.com>
2023-10-07 14:36:48 +0300

    Delete HW №2 f/#2.py

--- HW №2 f/#2.py
+++
@@ -1,8 +0,0 @@
-numbers = []                                  
-n = int(input("Введите количество целых чисел: "))
-for i in range(n):                             # цикл for для получения каждого числа которое ввел
-    num = int(input("Введите число: "))        
-    numbers.append(num)                        # добавил в numbers с помощью .append(в конец строки)
-numbers.sort(reverse=True)                     # sort сортирует cписок в порядке убывания,reserve меняет наоборот порядок эл-ов(н-р:первый станет последним)
-max_numb = int(''.join(map(str, numbers)))     # .join объединяет в 1 строку,map применяет str к каждому эл-у списка
-print(max_numb)       
